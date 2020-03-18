97da645fd9d386ac8a29b3e5e7eea07919c45f87
Diego Reis <52046178+diegolrs@users.noreply.github.com>
2020-03-18 18:52:10 -0300

    Calibração de cor

--- Detecção de cor/detectaCor.cpp
+++ Detecção de cor/detectaCor.cpp
@@ -5,19 +5,23 @@
 #include <opencv2/opencv.hpp>
 #include <iostream> 
 
+
+
 int main(int argc, const char** argv)
 {
 	cv::VideoCapture webCam(0);
 	cv::Mat frame;
 
-	const int FRAMES_MINIMOS_PARA_VALIDACAO = 130;
+	const int FRAMES_MINIMOS_PARA_VALIDACAO = 50;
 
 	int framesVermelhos, framesVerdes, framesAzuis;
 	framesVermelhos = framesVerdes = framesAzuis = 0;
 
+
 	if (!webCam.isOpened()) // Caso falhe em iniciar webam
+	{
 		return 1;
-	
+	}
 
 	while (webCam.read(frame)) // webCam >> frame
 	{
@@ -26,15 +30,15 @@ int main(int argc, const char** argv)
 		// BGR COLOR
 		// RED
 		cv::Mat OutputImage;
-		cv::inRange(frame, cv::Scalar(10, 10, 120), cv::Scalar(40, 40, 255), OutputImage);
+		cv::inRange(frame, cv::Scalar(10, 10, 100), cv::Scalar(50, 50, 255), OutputImage);
 
 		// BLUE
 		cv::Mat OutputImage2;
-		cv::inRange(frame, cv::Scalar(120, 10, 10), cv::Scalar(255, 40, 40), OutputImage2);
+		cv::inRange(frame, cv::Scalar(100, 10, 10), cv::Scalar(255, 40, 40), OutputImage2);
 
 		// GREEN
 		cv::Mat OutputImage3;
-		cv::inRange(frame, cv::Scalar(10, 120, 10), cv::Scalar(40, 255, 40), OutputImage3);
+		cv::inRange(frame, cv::Scalar(10, 100, 10), cv::Scalar(40, 255, 40), OutputImage3);
 
 
 		int x1, x2, x3;
@@ -90,5 +94,7 @@ int main(int argc, const char** argv)
 			break;
 		}
 	}
+
+
 	cv::waitKey(0);
 }
